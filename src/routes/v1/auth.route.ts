b20b901a0b8a1fa8f135d69e8c10c5abc462d9a4
yadda <yadda@weoneit.net>
2023-08-28 11:32:44 +0100

    chore: remove email route

--- src/routes/v1/auth.route.ts
+++ src/routes/v1/auth.route.ts
@@ -1,16 +1,33 @@
-import { Router } from 'express';
-import validate from '../../middleware/validate';
-import { loginSchema, signupSchema } from '../../validations/auth.validation';
-import * as authController from '../../controller/auth.controller';
+import { Router } from "express";
+import validate from "../../middleware/validate";
+import * as authController from "../../controller/auth.controller";
+import * as passwordController from "../../controller/forgotPassword.controller";
+import {
+  loginSchema,
+  signupSchema,
+  forgotPasswordSchema,
+  resetPasswordSchema,
+} from "../../validations/auth.validation";
 
 const authRouter = Router();
 
-authRouter.post('/signup', validate(signupSchema), authController.handleSignUp);
+authRouter.post("/signup", validate(signupSchema), authController.handleSignUp);
 
-authRouter.post('/login', validate(loginSchema), authController.handleLogin);
+authRouter.post("/login", validate(loginSchema), authController.handleLogin);
 
-authRouter.post('/logout', authController.handleLogout);
+authRouter.post("/logout", authController.handleLogout);
 
-authRouter.post('/refresh', authController.handleRefresh);
+authRouter.post("/refresh", authController.handleRefresh);
+
+authRouter.post(
+  "/forgot-password",
+  validate(forgotPasswordSchema),
+  passwordController.handleForgotPassword
+);
+authRouter.post(
+  "/reset-password/:token",
+  validate(resetPasswordSchema),
+  passwordController.handleResetPassword
+);
 
 export default authRouter;
--- src/validations/auth.validation.ts
+++ src/validations/auth.validation.ts
@@ -1,20 +1,36 @@
-import Joi from 'joi';
+import Joi from "joi";
 import type {
   UserLoginCredentials,
-  UserSignUpCredentials
-} from '../types/types';
+  UserSignUpCredentials,
+} from "../types/types";
+import type { EmailRequestBody } from "../types/types";
 
 export const signupSchema = {
   body: Joi.object<UserSignUpCredentials>().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(6),
-    username: Joi.string().required().min(2)
-  })
+    username: Joi.string().required().min(2),
+  }),
 };
 
 export const loginSchema = {
   body: Joi.object<UserLoginCredentials>().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(6)
-  })
+    password: Joi.string().required().min(6),
+  }),
+};
+
+export const forgotPasswordSchema = {
+  body: Joi.object<EmailRequestBody>().keys({
+    email: Joi.string().required().email(),
+  }),
+};
+
+export const resetPasswordSchema = {
+  body: Joi.object().keys({
+    newPassword: Joi.string().required().min(6),
+  }),
+  params: Joi.object().keys({
+    token: Joi.string().required().min(1),
+  }),
 };
