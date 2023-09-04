d2bf5a0868f66d94a72020d123fff99bfea84ee7
yadda <yadda@weoneit.net>
2023-09-04 10:49:54 +0100

    chore: catch async instead of try catch

--- src/controller/forgotPassword.controller.ts
+++ src/controller/forgotPassword.controller.ts
@@ -1,15 +1,20 @@
 import type { Response, Request } from "express";
 import httpStatus from "http-status";
 import * as authService from "../service/auth.service";
+import catchAsync from "../utils/catchAsync";
 
-export const handleForgotPassword = async (req: Request, res: Response) => {
-  const { email } = req.body;
-  const result = await authService.forgotPassword(email);
-  return res.status(httpStatus.OK).json(result);
-};
+export const handleForgotPassword = catchAsync(
+  async (req: Request, res: Response) => {
+    const { email } = req.body;
+    const result = await authService.forgotPassword(email);
+    return res.status(httpStatus.OK).json(result);
+  }
+);
 
-export const handleResetPassword = async (req: Request, res: Response) => {
-  const { password, token } = req.body;
-  const result = await authService.resetPassword(password, token);
-  return res.status(httpStatus.OK).json(result);
-};
+export const handleResetPassword = catchAsync(
+  async (req: Request, res: Response) => {
+    const { password, token } = req.body;
+    const result = await authService.resetPassword(password, token);
+    return res.status(httpStatus.OK).json(result);
+  }
+);
